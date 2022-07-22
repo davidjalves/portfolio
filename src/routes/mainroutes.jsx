abbc7f724e003449d93e5c6a6a254d58b47f6a5b
davidjalves <97697920+davidjalves@users.noreply.github.com>
2022-07-22 19:03:33 +0100

    minor changes

--- src/routes/mainroutes.jsx
+++ src/routes/mainroutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { Home, NotFound } from "../screens";
 import RoutesPublic from "./public/routesPublic";
 

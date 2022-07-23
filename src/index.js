04b5b7db44b9b2e1fb79beb60ef23f75c5bef843
davidjalves <97697920+davidjalves@users.noreply.github.com>
2022-07-23 18:38:06 +0100

    trying to correct error github pages refresh #2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MainRoutes from "./routes/mainroutes";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, HashRouter} from "react-router-dom";
 export const MAINROUTE = "/portfolio";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <HashRouter >
       <MainRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
